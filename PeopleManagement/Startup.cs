b06d6c8144f96d8e3b4f08122eb468ab826d9c07
Reinaldo Souza <reinaldo_7531@hotmail.com>
2024-01-07 18:29:06 +0000

    config startup to Login Page insted Home

--- PeopleManagement/Startup.cs
+++ PeopleManagement/Startup.cs
@@ -50,7 +50,7 @@ namespace PeopleManagement
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
