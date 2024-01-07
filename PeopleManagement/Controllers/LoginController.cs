5d91634983e06ddae93831b7b11dae71296cf5c8
Reinaldo Souza <reinaldo_7531@hotmail.com>
2024-01-07 18:19:08 +0000

    Login View

---
+++ PeopleManagement/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleManagement.Models;
+
+namespace PeopleManagement.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            UserViewModel user = new UserViewModel();
+            return View("Index",user);
+        }
+        [HttpPost]
+        public IActionResult Test(UserViewModel user)
+        {
+            user.Email = "email enviaDO";
+            return View("Index", user);
+        }
+    }
+}
