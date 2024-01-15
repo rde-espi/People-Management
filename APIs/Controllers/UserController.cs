0402ec740f83160b8b58c9a186eef68a5b5273ec
Reinaldo Souza <reinaldo_7531@hotmail.com>
2024-01-15 20:34:39 +0000

    JS introdution

---
+++ APIs/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Login(string username, string password)
+        {
+            return Ok(new {Response = "Esta Logado"});
+        }
+    }
+}
---
+++ PeopleManagement/wwwroot/js/Login.js
@@ -0,0 +1,12 @@
+﻿funtion Login(event){
+    event.preventDefault();
+
+    var formData = { email: $("#email").val(), password: $("#password".val()) }
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        contentType: "application/json; charset=UTF-8",
+        data:
+    })
+}
